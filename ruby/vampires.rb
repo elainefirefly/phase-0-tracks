5a2e763328b9862f5dada96c6965cc212069fe1b
Joanna Joseph <elainefirefly@gmail.com>
2017-03-06 12:44:12 -0800

    Complete release 2 of 4.4 detection logic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,10 +1,30 @@
+# Release 1
 puts "What is your name?"
 name = gets.chomp
 puts "How old are you?"
-age = gets.chomp
+age = gets.chomp.to_i
 puts "What year were you born?"
-birth_year = gets.chomp
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-order_garlic = gets.chomp
-puts "Would you like to enroll in the company's health insurance?"
-enroll_insurance = gets.chomp
+birth_year = gets.chomp.to_i
+puts "Our company cafeteria serves garlic bread. Should we order some for you? Type 'y' or 'n'."
+order_garlic = gets.chomp == 'y' ? true : false;
+puts "Would you like to enroll in the company's health insurance? Type 'y' or 'n'."
+enroll_insurance = gets.chomp == 'y' ? true : false;
+
+# Release 2
+correct_age = Time.now.year - birth_year == age
+result = nil
+if correct_age && order_garlic && enroll_insurance
+  result = "Probably not a vampire"
+elsif !correct_age && (!order_garlic ^ !enroll_insurance)
+  result = "Probably a vampire"
+elsif !correct_age && !order_garlic && !enroll_insurance
+  result = "Almost certainly a vampire"
+else
+  result = nil
+end
+
+result = "Definitely a vampire" if name == "Drake Cula" || name == "Tu Fang"
+
+result ||= "Results inconclusive"
+
+puts "Vampire Test Result for #{name}: #{result}"

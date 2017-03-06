6c56670e61ae64dde778ea825a41b459345ddab4
Joanna Joseph <elainefirefly@gmail.com>
2017-03-06 11:08:47 -0800

    Complete release 1 of 4.4 control flow

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.chomp
+puts "What year were you born?"
+birth_year = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+order_garlic = gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+enroll_insurance = gets.chomp

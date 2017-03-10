c3e807e89d63e94294202c91df0989e8ad7fbf65
Joanna Joseph <elainefirefly@gmail.com>
2017-03-09 20:27:50 -0800

    Complete release 1 of 4.3 gather data

---
+++ ruby/hamsters.rb
@@ -0,0 +1,16 @@
+# 4.3 Mandatory Pairing
+# Paired with Kiv Lo
+puts "What is your hamster's name?"
+hamster_name = gets.chomp
+
+puts "Is your hamster noisy? Rate between 1-10:"
+hamster_volume = gets.chomp
+
+puts "What is your hamster's fur color?"
+hamster_color = gets.chomp
+
+puts "Is your hamster a good candidate for adoption? (y/n)"
+hamster_behave = gets.chomp
+
+puts "What is your hamster's estimated age?"
+hamster_age = gets.chomp

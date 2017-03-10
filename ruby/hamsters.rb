eb6d9eac760e319d47d8bc1342bbd430f4533d45
Joanna Joseph <elainefirefly@gmail.com>
2017-03-09 20:35:24 -0800

    Process string input

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -4,13 +4,15 @@ puts "What is your hamster's name?"
 hamster_name = gets.chomp
 
 puts "Is your hamster noisy? Rate between 1-10:"
-hamster_volume = gets.chomp
+hamster_volume = gets.chomp.to_i
 
 puts "What is your hamster's fur color?"
 hamster_color = gets.chomp
 
 puts "Is your hamster a good candidate for adoption? (y/n)"
-hamster_behave = gets.chomp
+hamster_behave = gets.chomp == "y" ? true : false
 
 puts "What is your hamster's estimated age?"
 hamster_age = gets.chomp
+
+hamster_age = nil if hamster_age == ""

22c0a1c7922f9b2e5bbc938be6526d72ca614f7b
Joanna Joseph <elainefirefly@gmail.com>
2017-03-09 20:46:54 -0800

    Print to the console

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,5 +1,7 @@
 # 4.3 Mandatory Pairing
 # Paired with Kiv Lo
+
+# Get data
 puts "What is your hamster's name?"
 hamster_name = gets.chomp
 
@@ -14,5 +16,20 @@ hamster_behave = gets.chomp == "y" ? true : false
 
 puts "What is your hamster's estimated age?"
 hamster_age = gets.chomp
-
 hamster_age = nil if hamster_age == ""
+
+# Process data and print to the console
+puts "Hamster Bio"
+puts "Hamster name : #{hamster_name}"
+if hamster_volume >= 5
+	puts "Noise level : Jolly and cheerful and can keep you up all night"
+else
+	puts "Noise level: Quiet like a hamster ninja"
+end
+puts "Hamster' Fur color: #{hamster_color}"
+if hamster_behave
+	puts "Temperament: Sweet and lovely (adoptable!)"
+else
+	puts "Temperament: Will bounce off the wall (adopt at your own discretion!)"
+end
+puts "Hamster's estimated age : #{hamster_age}"

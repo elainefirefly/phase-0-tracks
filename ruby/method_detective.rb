c67383cf5a2f1d2cdf38b9ad266ff8ed6c6dbb09
Joanna Joseph <elainefirefly@gmail.com>
2017-03-12 18:49:32 -0700

    Fix missing parameter

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,62 +4,62 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-"iNvEsTiGaTiOn".swapcase
+puts "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-"zom".insert(1,'o')
+puts "zom".insert(1,'o')
 # => “zoom”
 
-"zom".sub("o","oo")
+puts "zom".sub("o","oo")
 # => “zoom”
 
-"zom".gsub("o","oo")
+puts "zom".gsub("o","oo")
 # => “zoom”
 
-"enhance".center(15)
+puts "enhance".center(15)
 # => "    enhance    "
 
-"Stop! You’re under arrest!".upcase
+puts "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-"the usual".concat(" suspects")
+puts "the usual".concat(" suspects")
 #=> "the usual suspects"
 
-"the usual".insert(-1, " suspects")
+puts "the usual".insert(-1, " suspects")
 #=> "the usual suspects"
 
-" suspects".prepend("the usual")
+puts " suspects".prepend("the usual")
 # => "the usual suspects"
 
-" suspects".insert(0, "the usual")
+puts " suspects".insert(0, "the usual")
 # => "the usual suspects"
 
-"The case of the disappearing last letter".chop
+puts "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-"The case of the disappearing last letter".slice(0...-1)
+puts "The case of the disappearing last letter".slice(0...-1)
 # => "The case of the disappearing last lette"
 
-"The mystery of the missing first letter".slice(1..-1)
+puts "The mystery of the missing first letter".slice(1..-1)
 # => "he mystery of the missing first letter"
 
-"The mystery of the missing first letter".sub("T","")
+puts "The mystery of the missing first letter".sub("T","")
 # => "he mystery of the missing first letter"
 
-"Elementary,    my   dear        Watson!".squeeze
+puts "Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
-"Elementary,    my   dear        Watson!".gsub(/\ +/," ")
+puts "Elementary,    my   dear        Watson!".gsub(/\ +/," ")
 # => "Elementary, my dear Watson!"
 
-"z".ord
+puts "z".ord
 # => 122
 
-"z".getbyte
+puts "z".getbyte(0)
 # => 122
 
 # (What is the significance of the number 122 in relation to the character z?)
-# 122 is the decimal ASCII code for the character z
+puts "122 is the decimal ASCII code for the character z"
 
-"How many times does the letter 'a' appear in this string?".count("a")
+puts "How many times does the letter 'a' appear in this string?".count("a")
 # => 4

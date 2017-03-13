0bb2e7aba0c2638bea69865b62f976d2dd4c35f0
Joanna Joseph <elainefirefly@gmail.com>
2017-03-12 18:43:31 -0700

    Complete release 5.1

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,62 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+"zom".insert(1,'o')
 # => “zoom”
 
-# "enhance".<???>
+"zom".sub("o","oo")
+# => “zoom”
+
+"zom".gsub("o","oo")
+# => “zoom”
+
+"enhance".center(15)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+"the usual".concat(" suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+"the usual".insert(-1, " suspects")
+#=> "the usual suspects"
+
+" suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+" suspects".insert(0, "the usual")
+# => "the usual suspects"
+
+"The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+"The case of the disappearing last letter".slice(0...-1)
+# => "The case of the disappearing last lette"
+
+"The mystery of the missing first letter".slice(1..-1)
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"The mystery of the missing first letter".sub("T","")
+# => "he mystery of the missing first letter"
+
+"Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+"Elementary,    my   dear        Watson!".gsub(/\ +/," ")
+# => "Elementary, my dear Watson!"
 
-# "How many times does the letter 'a' appear in this string?".<???>
+"z".ord
+# => 122
+
+"z".getbyte
+# => 122
+
+# (What is the significance of the number 122 in relation to the character z?)
+# 122 is the decimal ASCII code for the character z
+
+"How many times does the letter 'a' appear in this string?".count("a")
 # => 4

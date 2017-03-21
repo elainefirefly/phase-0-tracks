2a861046cb1a123aa160d0bb45b05f56ae48a877
Joanna Joseph <elainefirefly@gmail.com>
2017-03-20 19:26:03 -0700

    Complete release 2 of 6.3 change attribute w/ method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,5 @@
 class Santa
-  attr_reader :gender, :ethnicity
+  attr_reader :gender, :reindeer_ranking
 
   def initialize(gender,ethnicity)
     puts "Initializing Santa instance..."
@@ -17,19 +17,64 @@ class Santa
     puts "That was a good #{cookie}!"
   end
 
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at(name)
+    @reindeer_ranking.delete(name)
+    @reindeer_ranking.push(name)
+  end
+
+  def gender=(gender)
+    @gender = gender
+  end
+
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end
+
 end
 
-# release 1 driver code:
+# release 1
 santa_fam = []
 genders = %w{female male bisexual gay lesbian transgender queer}
 ethnicity = %w{White Black Native\ American Alaska\ Native Hispanic Asian\ American Native\ Hawaiian Pacific\ Islander}
 9.times { santa_fam << Santa.new(genders[rand(7)], ethnicity[rand(8)]) }
+
+# release 1 driver code: (added attr_reader for testing)
 puts "Santa List:"
 santa_fam.each_with_index { |santa,idx| puts "#{idx + 1}. A #{santa.gender} #{santa.ethnicity} santa is born." }
 
+# release 2 driver code:
+#assign age to each santa
+santa_fam.each do |santa|
+  #due to some mystical cookie maturity booster
+  rand(99).times { santa.celebrate_birthday }
+  #queers are undecided, so they will likely decide eventually
+  santa.gender = genders[rand(6)] if santa.gender == "queer"
+  #sudden growth spurt can cause dangerous mood swings...
+  if santa.age > 60
+    santa.get_mad_at(santa.reindeer_ranking[rand(7)])
+    puts "REINDEER RANKING ALERT!!!"
+    santa.reindeer_ranking.each_with_index { |reindeer,idx| puts "Rank #{idx+1} #{reindeer}" }
+  end
+end
+
+puts "Revised Santa List:"
+santa_fam.each_with_index { |santa,idx| puts "#{idx + 1}. A #{santa.gender} #{santa.ethnicity} santa turned #{santa.age} years old." }
+
+
+
 =begin
 # release 0 driver code:
+
 smelly_santa = Santa.new
 smelly_santa.speak
 smelly_santa.eat_milk_and_cookies("dark choco macadamia cookie")
+
 =end

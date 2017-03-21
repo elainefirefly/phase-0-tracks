0d67ae7dae83ecc9ad6a9816274d4f31c644f902
Joanna Joseph <elainefirefly@gmail.com>
2017-03-20 18:01:20 -0700

    Complete release 1 of 6.3 give santa attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,12 @@
 class Santa
+  attr_reader :gender, :ethnicity
 
-  def initialize
+  def initialize(gender,ethnicity)
     puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = %w{Rudolph Dasher Dancer Prancer Vixen Comet Cupid Donner Blitzen}
+    @age = 0
   end
 
   def speak
@@ -14,7 +19,17 @@ class Santa
 
 end
 
-# driver code:
+# release 1 driver code:
+santa_fam = []
+genders = %w{female male bisexual gay lesbian transgender queer}
+ethnicity = %w{White Black Native\ American Alaska\ Native Hispanic Asian\ American Native\ Hawaiian Pacific\ Islander}
+9.times { santa_fam << Santa.new(genders[rand(7)], ethnicity[rand(8)]) }
+puts "Santa List:"
+santa_fam.each_with_index { |santa,idx| puts "#{idx + 1}. A #{santa.gender} #{santa.ethnicity} santa is born." }
+
+=begin
+# release 0 driver code:
 smelly_santa = Santa.new
 smelly_santa.speak
 smelly_santa.eat_milk_and_cookies("dark choco macadamia cookie")
+=end

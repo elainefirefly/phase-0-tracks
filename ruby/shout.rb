79cfdf8db05f3485d4297239a86477cab9329262
Joanna Joseph <elainefirefly@gmail.com>
2017-03-22 12:00:47 -0700

    Fix create list method for gps 2_2

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,38 @@
+# release 3
+module Shout
+  def yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def yell_happily(words)
+    words.upcase + "! :)"
+  end
+
+end
+
+class Husband
+  include Shout
+end
+
+class Wife
+  include Shout
+end
+
+# driver code
+hubby = Husband.new
+hubby.yell_angrily("What? Another purse")
+wifey = Wife.new
+wifey.yell_happily("It's Chanel")
+
+hubby.yell_happily("It's a boy")
+wifey.yell_angrily("")
+
+
+
+
+
+
+=begin
 module Shout
   def self.yell_angrily(words)
     words + "!!!" + " :("
@@ -11,3 +46,4 @@ end
 # driver code
 puts Shout.yell_angrily("Get lost")
 puts Shout.yell_happily("Congrats")
+=end

e7a2c0947c50a23747f408adb28e87e86e7023e4
Joanna Joseph <elainefirefly@gmail.com>
2017-03-22 19:33:08 -0700

    Complete release 3 of 6.4 convert to mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -7,7 +7,16 @@ module Shout
   def yell_happily(words)
     words.upcase + "! :)"
   end
+end
 
+#just playing, not required by module
+module Borderize
+  def self.create_border(unicode1,unicode2)
+    puts
+    10.times { print " #{unicode1} #{unicode2} " }
+    puts
+    puts
+  end
 end
 
 class Husband
@@ -20,19 +29,17 @@ end
 
 # driver code
 hubby = Husband.new
-hubby.yell_angrily("What? Another purse")
 wifey = Wife.new
-wifey.yell_happily("It's Chanel")
-
-hubby.yell_happily("It's a boy")
-wifey.yell_angrily("")
-
-
-
-
-
+Borderize.create_border("\u{1f468}", "\u{1f469}")
+puts hubby.yell_angrily("Hubby: What? Another purse")
+puts wifey.yell_happily("Wifey: It's Chanel")
+Borderize.create_border("\u{1f468}", "\u{1f469}")
+puts hubby.yell_happily("Hubby: Happy Anniversary")
+puts wifey.yell_angrily("Wifey: A vacuum cleaner – you're giving me a vacuum cleaner?")
+Borderize.create_border("\u{1f468}", "\u{1f469}")
 
 =begin
+# release 1
 module Shout
   def self.yell_angrily(words)
     words + "!!!" + " :("

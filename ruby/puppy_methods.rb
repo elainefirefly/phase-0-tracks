a6ee3962922e114b518bb2e601461b1f81179204
Joanna Joseph <elainefirefly@gmail.com>
2017-03-22 21:59:33 -0700

    Complete release 0 of 6.2 add behavior to class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,32 @@
+# release 0
 class Puppy
-
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
 
+  def speak(number)
+    number.times { puts "Woof!"}
+  end
+
+  def roll_over
+    puts "rolls over"
+  end
+
+  def dog_years(human_years)
+    human_years * 7
+  end
+
+  def lick_face(name)
+    puts "Licks #{name}'s face \u{1f436}"
+  end
 end
 
+
+# driver code
+cinnamon = Puppy.new
+cinnamon.fetch("shuriken")
+cinnamon.speak(5)
+cinnamon.roll_over
+puts cinnamon.dog_years(1)
+cinnamon.lick_face("Jojo")

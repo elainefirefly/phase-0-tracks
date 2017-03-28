a3d98a7fd756f00151c9a4c02befc58f7084dc1e
Joanna Joseph <elainefirefly@gmail.com>
2017-03-28 14:21:45 -0700

    Complete release 2 use constructor to build object

--- js/data_structures.js
+++ js/data_structures.js
@@ -12,3 +12,23 @@ for (var i = 0; i < names.length; i++ ) {
 }
 
 console.log(horses);
+
+function Car(make, model, year, convertible) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.convertible = convertible;
+  this.start = function() {console.log("vroooom vroom!");};
+}
+
+// driver
+var carOne = new Car("Toyota", "Camry", 2012, false);
+var carTwo = new Car("Ford", "Pinto", 1970, false);
+var carThree = new Car("Shelby", "Cobra", 1965, true);
+var carFour = new Car("Bugatti", "Veyron", 2011, false);
+
+var carArray = [carOne, carTwo, carThree, carFour];
+
+for (var i = 0; i < carArray.length; i++) {
+  console.log(carArray[i]);
+}

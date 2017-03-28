9d43cb9370e9b276a6ad891d58ca94bd505a445d
Joanna Joseph <elainefirefly@gmail.com>
2017-03-28 13:46:46 -0700

    Complete release 1 of 7.1 build an object

--- js/data_structures.js
+++ js/data_structures.js
@@ -3,3 +3,12 @@ var names = ["Skyla", "Beauty", "Zed", "Trump"];
 
 colors.push("pink");
 names.push("Becky");
+
+var horses = {};
+
+for (var i = 0; i < names.length; i++ ) {
+  horses[names[i]] = colors[i];
+  //console.log(names[i] + " " + horses[names[i]]);
+}
+
+console.log(horses);

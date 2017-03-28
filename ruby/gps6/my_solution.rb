9fddad1298f6ba885041cfabd1c567708cf74a43
Joanna Joseph <elainefirefly@gmail.com>
2017-03-27 20:15:56 -0700

    Complete release 8 of gps 3.1 reflection

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -58,8 +58,30 @@ end
 
 # DRIVER CODE
  # initialize VirusPredictor for each state
-STATE_DATA.each do |state,report|
-  VirusPredictor.new(state, report[:population_density], report[:population]).virus_effects
-end
+STATE_DATA.each { |state,report| VirusPredictor.new(state, report[:population_density], report[:population]).virus_effects }
 #=======================================================================
 # Reflection Section
+=begin
+What are the differences between the two different hash syntaxes shown in the state_data file?
+The outer hash uses a rocket syntax and string as key, the inner hash uses the colon syntax using a symbol as a key. The symbol key is immutable and not garbage collected, so using it has pros and cons depending on what I will be using it for.
+
+What does require_relative do? How is it different from require?
+require_relative is a syntactic sugar for require and current folder path.
+Unlike require, if it does not find the file in the current folder, it
+will immediately give you a load error.  Require will require the path
+and if it does not find it in that path, it will look in the list of paths
+in $LOAD_PATH.
+
+What are some ways to iterate through a hash?
+There are a number of ways to iterate through a hash depending on what you are trying to achieve and the data that you are trying to extract. One can iterate using a
+enumerables like .each, .select, .each_key, .each_pair, or the lesser Ruby-like way of a for loop.
+
+When refactoring virus_effects, what stood out to you about the variables, if anything?
+Assignment to variables are repeated many times throughout each method, which was unnecessary.  Also, ivars do not need to be passed around since they have the scope of the class definition.
+It also uses unnecessary operation like += when the result is the same as =.
+The rounding of the numbers can be done when the final calculation is made and the final value is reached.
+This is a good example of a working code, but not a clever and DRY one.
+
+What concept did you most solidify in this challenge?
+Iterating through data structure makes so much sense especially when dealing with big data.  Also, when I think that a code looks lean, it really is not, I can further shrink it down and make it better.
+=end

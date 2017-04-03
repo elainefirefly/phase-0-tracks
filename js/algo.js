906f277c90164a3467591aae49009b413f195b57
Joanna Joseph <elainefirefly@gmail.com>
2017-04-02 19:46:47 -0700

    Complete release 2 of 7.3 generate random test data

--- js/algo.js
+++ js/algo.js
@@ -50,6 +50,83 @@ function findIdenticalPair(object1, object2) {
   return false;
 }
 
+// release 2
+/*
+IMPORTANT: I slightly made this release a bit more challenging by creating phrases instead of words.
+I did this because I chose phrase as input for release 0
+Pseudocode:
+Define function that generates a random phrase that takes an array and the required number of phrases
+FUNCTION
+  LOOP to create the required number of phrases
+    Create an empty array
+    Determine the number of words for the phrase
+    FOR the number of words
+      Call the helper function that generates a word
+      Put the word inside the empty array
+    END FOR
+    Connect all the words in the array with a space in between
+    Put the string result inside the array passed as parameter to the function
+  END LOOP
+END FUNCTION
+*/
+
+function generateRandomPhrase(phraseArray, numberOfPhrase) {
+	for (let i = 0; i < numberOfPhrase; i++) {
+		phrase = [];
+		numOfWords = Math.floor(Math.random() * 5) + 1;
+		for (let j = 0; j < numOfWords; j++) {
+			phrase.push(generateWord());
+		}
+		phraseArray.push(phrase.join(" "));
+	}
+}
+
+/*
+Pseudocode:
+Define a function that generates a word
+FUNCTION
+  Generate a random word length
+  Create a min variable to store the character code for "a"
+  Create a max variable to store the character code for "z"
+  Get the difference of the max and min variables
+  Create an empty string called word
+  LOOP using the number of letters required for the word
+    Generate a string from the randomized char code and concatenate it to the word string
+  END LOOP
+  RETURN word
+END FUNCTION
+*/
+
+function generateWord() {
+	wordLength = Math.floor(Math.random() * 10 + 1);
+	var min = "a".charCodeAt();
+	var max = "z".charCodeAt();
+	var diff = max - min;
+	var word = "";
+	for (let i = 0; i < wordLength; i++) {
+		word += String.fromCharCode(Math.floor(Math.random(min,max) * diff) + min);
+	}
+	return word;
+}
+
+/*
+Pseudocode:
+Define a function to print the generated phrases that accepts the array of phrases
+FUNCTION
+  Print on console a heading of "The phrases generated are:"
+  FOR each phrase of the array of phrases
+    Print each phrase on the console
+  END FOR
+END FUNCTION
+*/
+
+function printGeneratedPhrases(phrases) {
+	console.log("\n\nThe phrases generated are:")
+	for (let phrase of phrases) {
+		console.log(phrase);
+	}
+}
+
 // driver code:
 // release 0
 console.log(findLongestPhrase(["long phrase", "longest phrase", "longer phrase"]));
@@ -65,3 +142,11 @@ var kingstonBoot = {"color": "olive", price: 172.00, material: "suede"};
 console.log(findIdenticalPair(iPhone6s, saddleBag));
 console.log(findIdenticalPair(iPhone6s, kingstonBoot));
 console.log(findIdenticalPair(kingstonBoot, saddleBag));
+
+// release 2
+for (let i = 0; i < 10; i++) {
+	var phraseArray = [];
+	generateRandomPhrase(phraseArray, Math.floor(Math.random() * 10) + 1);
+	printGeneratedPhrases(phraseArray);
+	console.log("\nLongest phrase: " + findLongestPhrase(phraseArray));
+}

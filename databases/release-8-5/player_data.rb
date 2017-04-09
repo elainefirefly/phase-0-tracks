d9349e9637f2333a6006ee95aee0cd2603908e05
Joanna Joseph <elainefirefly@gmail.com>
2017-04-09 03:01:16 -0700

    Implement update_stats method

--- databases/release-8-5/player_data.rb
+++ databases/release-8-5/player_data.rb
@@ -63,7 +63,20 @@ class PlayerData
     @userid = ("SELECT id FROM players WHERE username=\"#{@username}\";")
   end
 
-  def save_stats
+  def update_stats(new_info)
+    new_info.each do |key,value|
+      next if key == "id" || key == "username" || value.nil?
+      break if key.is_a? Integer
+      if value.is_a? String
+        puts update_str = "UPDATE players SET #{key}=\"#{value}\" WHERE username=\"#{@username}\";"
+      else
+        puts update_str = "UPDATE players SET #{key}=#{value} WHERE username=\"#{@username}\";"
+      end
+      @db.execute(update_str)
+    end
+  end
+
+  def pause_game
     
   end
 end
@@ -76,4 +89,7 @@ else
   puts false
 end
 puts playerdata.game_paused?
+puts playerdata.get_stats
+hash_info = {"id"=>1, "username"=>"Cocochanel", "easy_streak"=>25, "med_streak"=>25, "hard_streak"=>15, "general_questions"=>10, "general_correct"=>20, "computer_questions"=>nil, "computer_correct"=>nil, "history_questions"=>nil, "history_correct"=>16, "paused_game"=>1, 0=>1, 1=>"Cocochanel", 2=>nil, 3=>nil, 4=>nil, 5=>nil, 6=>nil, 7=>nil, 8=>nil, 9=>nil, 10=>nil, 11=>nil}
+playerdata.update_stats(hash_info)
 puts playerdata.get_stats

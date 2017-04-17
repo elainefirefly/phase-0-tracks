b63263b3c0eed6a466d076384b56c8621fb348c4
Joanna Joseph <elainefirefly@gmail.com>
2017-04-16 23:15:43 -0700

    Create js file and add click event

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,3 +1,8 @@
-function showGifs() {
+console.log("Connected");
 
+function showGifs() {
+  dramaCover.nextSibling.toggle('visible');
 }
+
+var dramaCover = document.getElementsByClassName('cover');
+dramaCover.addEventListener("click", showGifs);
